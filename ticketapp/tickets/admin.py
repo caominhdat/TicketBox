7bc59e1d37e2587915d5246c0d08ffd15398e3c0
Cao Minh Đạt <caominhdat@192.168.111.148.non-exists.ptr.local>
2025-03-31 20:05:24 +0700

    update model

--- ticketapp/tickets/admin.py
+++ ticketapp/tickets/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Event
+from .models import Category, Event, TypeTicket, Ticket, Chat, Discount, Payment, CheckIn, Review, Notification
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['pk', 'name']
@@ -9,3 +9,11 @@ class CategoryAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Event)
+admin.site.register(TypeTicket)
+admin.site.register(Ticket)
+admin.site.register(Chat)
+admin.site.register(Discount)
+admin.site.register(Payment)
+admin.site.register(CheckIn)
+admin.site.register(Review)
+admin.site.register(Notification)
--- ticketapp/tickets/models.py
+++ ticketapp/tickets/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import CharField
 
 
 class User(AbstractUser):
@@ -12,7 +13,7 @@ class BaseModel(models.Model):
     active = models.BooleanField(default=True)
 
     class Meta:
-        abstract = True #lop truu tuong khong tao instance
+        abstract = True  # lop truu tuong khong tao instance
 
 
 class Category(BaseModel):
@@ -24,9 +25,91 @@ class Category(BaseModel):
 
 class Event(BaseModel):
     name = models.CharField(max_length=100, null=False)
-    description =models.TextField()
+    description = models.TextField()
     image = models.CharField(max_length=100)
-    category =models.ForeignKey(Category, on_delete=models.CASCADE) # Category xoa thi event xoa
+    address = models.CharField(max_length=100, null=True)
+    map = models.CharField(max_length=100, null=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True)  # Category xoa thi event xoa
+    organizer = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.name
+
+
+class TypeTicket(BaseModel):
+    name = models.CharField(max_length=100, null=False)
+    quantity = models.PositiveIntegerField()
+    price = models.BigIntegerField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)  # Event xoa thi TypeTicket xoa
+
+    def __str__(self):
+        return self.name
+
+
+class Discount(BaseModel):
+    name = models.CharField(max_length=50, null=False)
+    code = models.CharField(max_length=50, null=False)
+    percent = models.IntegerField()
+    max_use = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Payment(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    event = models.ForeignKey(Event, on_delete=models.PROTECT)
+    type_ticket = models.ForeignKey(TypeTicket, on_delete=models.PROTECT)
+    status = models.BooleanField(default=False)
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.status
+
+
+class Ticket(BaseModel):
+    qr_code = models.CharField(max_length=200, null=False)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    event = models.ForeignKey(Event, on_delete=models.PROTECT)
+    type_ticket = models.ForeignKey(TypeTicket, on_delete=models.PROTECT)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.qr_code
+
+
+class CheckIn(BaseModel):
+    ticket = models.ForeignKey(Ticket, models.CASCADE)
+    event = models.ForeignKey(Event, models.CASCADE)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.status
+
+
+class Review(BaseModel):
+    rating = models.IntegerField(default=5)
+    comment = models.TextField()
+    user = models.ForeignKey(User, models.CASCADE)
+    event = models.ForeignKey(Event, models.CASCADE)
+
+    def __str__(self):
+        return self.rating
+
+
+class Notification(BaseModel):
+    title = models.CharField(max_length=100, null=False)
+    message = models.TextField()
+    user = models.ForeignKey(User, models.PROTECT)
+    event = models.ForeignKey(Event, models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+
+class Chat(BaseModel):
+    message = models.TextField()
+    user = models.ForeignKey(User, models.CASCADE)
+
+    def __str__(self):
+        return self.message
